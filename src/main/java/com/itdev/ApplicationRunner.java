55461a3f00c56b7f8bcf111a7dbc4efc5724d9e6
IiLuha <elyamin@bk.ru>
2025-10-27 19:33:45 +0300

    Initial commit

---
+++ src/main/java/com/itdev/ApplicationRunner.java
@@ -0,0 +1,14 @@
+package com.itdev;
+
+import com.itdev.listener.OperationsConsoleListener;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext("com.itdev");
+//        OperationsConsoleListener listener = context.getBean(OperationsConsoleListener.class);
+//        listener.doListen(args);
+    }
+}
