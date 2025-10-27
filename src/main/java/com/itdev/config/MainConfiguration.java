fdd2e705978271f540c1906c6f804111e176f5a5
IiLuha <elyamin@bk.ru>
2025-10-27 19:39:17 +0300

    Add config

---
+++ src/main/java/com/itdev/config/MainConfiguration.java
@@ -0,0 +1,11 @@
+package com.itdev.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+//@EnableAspectJAutoProxy
+@PropertySource("classpath:application.properties")
+public class MainConfiguration {
+}
